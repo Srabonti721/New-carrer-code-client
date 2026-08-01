6c692ac15b0f737795be98713434426106d267f1
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-08-02 00:41:53 +0600

    using motion design banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { motion } from "motion/react"
+import team1 from '../../assets/Teams/developer.jpg';
+import team2 from '../../assets/Teams/softdev.jpg';
 
 const Banner = () => {
     return (
-<div className="hero bg-base-200">
+<div className="hero bg-base-200 min-w-96">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
+<div className='flex-1'>
+        <motion.img 
+animate={{
+    y:[0, 100, 0]
+}}
+transition={{duration:10, repeat:Infinity}}
+      src={team1}
+      className="max-w-sm  border-s-8 border-b-8 border-blue-500 rounded-t-3xl rounded-br-3xl shadow-2xl"
     />
-    <div>
+    <motion.img 
+    animate={{x:[100, 150, 100]}}
+    transition={{duration:10 , delay:5, repeat:Infinity}}
+      className='max-w-sm border-s-8 border-b-8 border-blue-500 rounded-t-3xl rounded-br-3xl' src={team2} alt="team2" />
+</div>
+    <div className='flex-1'>
       <motion.h1
       initial={{scale:0}}
        animate={{
